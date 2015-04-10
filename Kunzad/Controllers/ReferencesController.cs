d3f05d1c616b54cf3109bdcb7e9f7369e2469812
DandySembrano <dtszero_cool@yahoo.com>
2015-04-10 18:31:33 +0800

    add controller

---
+++ Kunzad/Controllers/ReferencesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kunzad.Controllers
+{
+    public class ReferencesController : Controller
+    {
+        // GET: References
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult _CustomerGroups()
+        {
+            ViewBag.PanelTitle = "Customer Group";
+            return View();
+        }
+
+        public ActionResult _Airlines()
+        {
+            return View();
+        }
+
+        public ActionResult _DataTables()
+        {
+            return View();
+        }
+
+
+    }
+}
