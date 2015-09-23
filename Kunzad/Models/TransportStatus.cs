1dfbb8fccd4229e57f86c30a62b9825858ac1bf2
Jason Caranzo <caranzojason@gmail.com>
2015-09-23 10:19:10 +0800

    commit transport status

---
+++ Kunzad/Models/TransportStatus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kunzad.Models
+{
+     static class Status
+    {
+
+         public enum TransportStatus
+         {
+             Open = 10,
+             Partial = 20,
+             Dispatch = 30,
+             Close = 40,
+             Cancel = 50
+         }
+
+         public enum TruckingType
+         {
+            PickUp = 10,
+            TruckingDelivery = 20
+         }
+
+         public enum TruckingStatus { 
+            Dispatch = 10,
+            Waybill = 20,
+            DeliveryUpdate = 30,
+            Cancelled = 40
+         }
+    }
+}
