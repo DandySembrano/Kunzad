f88f80eb6885843607d9c541915a266c9c7fd9be
Rey P. Castanares <rpcastanares@fastgroup.biz>
2015-10-06 10:06:34 +0800

    commit air freight module

---
+++ Kunzad/SCripts/app/airfreight-ctrl.js
@@ -0,0 +1,35 @@
+﻿
+kunzadApp.controller("AirFreightsController", AirFreightsController);
+function AirFreightsController($scope, $http, $interval, $filter, $rootScope) {
+    $scope.modelName = "Air Freight";
+    $scope.modelhref = "#/airfreight";
+    $scope.isPrevPage = false;
+    $scope.isNextPage = true;
+    $scope.actionMode = "Create";
+    $scope.selected = null;
+    $scope.currentPage = 1;
+    $scope.viewOnly = false;
+    $scope.isError = false;
+    $scope.errorMessage = "";
+    $scope.submitButtonText = "Submit";
+    $scope.tabPages = ["Information", "List"];
+    $scope.selectedTab = "Information";
+    $scope.showMenu = false;
+    $scope.truckingList = [];
+    $scope.businessUnitList = [];
+    $scope.trkgDeliveryList = [];
+    var pageSize = 20;
+
+   
+    // Initialization routines
+    var init = function () {
+        //$scope.focusOnTop();
+        //$scope.initTruckingItem();
+        //$scope.initBusinessUnits();
+        //$scope.loadDataTruckings();
+        //$scope.loadData($scope.currentPage);
+        //$scope.addNewBooking();
+    };
+
+    init();
+};
